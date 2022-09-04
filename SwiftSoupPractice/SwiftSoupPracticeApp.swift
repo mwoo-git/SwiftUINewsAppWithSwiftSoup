bae725e45f2c3e799216652d44ff3f215d83b083
WooMin <hanulbom@gmail.com>
2022-09-04 14:02:24 +0900

    Initial Commit

---
+++ SwiftSoupPractice/SwiftSoupPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSoupPracticeApp.swift
+//  SwiftSoupPractice
+//
+//  Created by Woo Min on 2022/09/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSoupPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
